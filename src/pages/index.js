7378da4c1cfc117b834029fe1cfb2450c57dc797
Vishnu <vishnujishnu0987@gmail.com>
2023-02-03 15:39:45 -0800

    header created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
            <main>
-            <p className='text-center'>Hello react</p>
+            <Header/>
             </main>
     </>
   )
